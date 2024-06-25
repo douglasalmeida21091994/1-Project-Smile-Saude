83b50b747ab4594f30ceb07cf9c3533e0252931b
Douglas Almeida <110480637+douglasalmeida21091994@users.noreply.github.com>
2024-06-25 04:04:29 -0300

    ajustes CSS HTML e JS

--- js/script.js
+++ js/script.js
@@ -1,23 +1,40 @@
-document.getElementById("read-link").addEventListener("click", function(event) {
-    event.preventDefault();
-    document.getElementById("mensagem").style.display = "none";
-    document.getElementById("container").style.display = "flex";     
-
+document.querySelector(".checkbox-wrapper input[type='checkbox']").addEventListener("change", function() {
     getIP();
-});
-
-document.getElementById("confirm-check").addEventListener("change", function() {
-    const downloadButtons = document.querySelectorAll('.download-btn');    
-
+    const checkConfirma = document.querySelector(".button");
     if (this.checked) {
-        downloadButtons.forEach(button => button.classList.remove("disabled"));        
-    } else {
-        downloadButtons.forEach(button => button.classList.add("disabled"));  
-          
+        this.disabled = true; // Desativar o checkbox quando marcado
+        checkConfirma.classList.remove("disabled2");
     }
 });
 
-// Exemplo de quando vai pegar o IP
+document.querySelector(".button").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("mensagem").style.display = "none";
+    document.getElementById("container").style.display = "flex";
+});
+
+document.getElementById("confirm-check").addEventListener("change", function(event) {
+    event.preventDefault(); // Prevenir ação padrão de mudança
+
+    const downloadButtons = document.querySelectorAll('.download-btn');
+
+    // Garantir que o checkbox não possa ser desmarcado depois de marcado
+    if (this.checked) {
+        this.disabled = true; // Desativar checkbox quando marcado
+        downloadButtons.forEach(button => button.classList.remove("disabled"));
+    } else {
+        this.checked = true; // Forçar o checkbox a permanecer marcado
+        downloadButtons.forEach(button => button.classList.add("disabled"));
+    }
+
+    // Manipular o histórico do navegador para evitar recarregamento da página
+    // window.history.pushState(null, null, window.location.href);
+    // window.addEventListener('popstate', function () {
+    //     window.history.pushState(null, null, window.location.href);
+    // });
+});
+
+// Exemplo de função para obter o IP
 function getIP() {
     let pegandoData = new Date();
     let IP = "192.182.123.145";
@@ -25,3 +42,12 @@ function getIP() {
     let cliente = "Douglas Almeida";
     console.log(`Carteirinha: ${carteirinha} | Associado: ${cliente} | Endereço IP: ${IP} | Data: ${pegandoData}`);
 }
+
+// Impedir desmarcação do checkbox ao clicar
+document.addEventListener("DOMContentLoaded", function() {
+    const confirmCheck = document.getElementById("confirm-check");
+    confirmCheck.addEventListener("mousedown", function(event) {
+        event.preventDefault();
+        return false;
+    });
+});
